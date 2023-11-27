d837b3f44496a27ec9f31df18f6ecdaec03ab817
Franck Gaudin <franck.gaudin@workleap.com>
2023-11-27 11:29:22 -0500

    add test app for treeshaking

--- packages/latte/src/index.ts
+++ packages/latte/src/index.ts
@@ -2,3 +2,5 @@ export { Button } from "./button/Button.tsx";
 export { Avatar } from "./avatar/Avatar.tsx";
 export { Tabs } from "./tabs/Tabs.tsx";
 export { TabsConf } from "./tabs/TabsConf.tsx";
+export { Select } from "./select/Select.tsx";
+export { List } from "./list/List.tsx";
