7782d443c53bd9707d3cfc639e47963e98af65d9
Franck Gaudin <franck.gaudin@workleap.com>
2023-11-27 15:53:34 -0500

    test treeshaking of lib

--- packages/latte/tsup.build.ts
+++ packages/latte/tsup.build.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-    entry: ["./src/*.(ts|tsx)"],
+    entry: ["./src/**/*.(ts|tsx)", "!./src/**/*.stories.(ts|tsx)"],
     format: ["esm"],
+    clean: true,
+    dts: true,
     minify: true,
-    splitting: false,
-    clean: true
+    splitting: true,
+    target: "esnext"
 });
